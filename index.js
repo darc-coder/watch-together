4707ea2890114a58b6ff14b808484a9aa43dfa19
Nitin <akashlakra789@gmail.com>
2021-08-02 02:09:26 +0530

    Partial App Creaed

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
+const pug = require('pug');
+const path = require("path");
 const app = express()
-const port = process.env.port || 3000
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: '*' } });
+
+const port = process.env.PORT || 3000
+
+var globalSocket = new Set();
+
+app.use(express.static(path.join(__dirname + "/public")))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+io.on('connection', (socket) => {
+    console.log(socket.id)
+
+    socket.on('myplaySeconds', playSeconds => {
+        socket.broadcast.emit('playSeconds', playSeconds);
+    })
+
+    socket.on('playerState', (currentState, id) => {
+        socket.broadcast.emit('playerState', currentState, id);
+    })
+});
+
+
 // use params
 app.get('/watch', (req, res) => {
-
+    res.sendFile(path.join(__dirname + "/public/watch.html"))
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+server.listen(port, () => {
+    console.log(`listening on localhost:${port}`);
+});
